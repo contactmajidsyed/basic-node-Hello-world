4eefc572ee0360066417f1fa6c3a917630718f47
Majid Syed <msyed@enova.com>
2018-10-04 20:59:12 -0500

    added server and web page
    added index.html for hello world page
    added simple server to load index.html

--- index.js
+++ index.js
@@ -1,4 +1,32 @@
-function hello() {
-    console.log("hello to the world")
-}
-hello();
+var http = require("http");
+var fs = require("fs");
+
+http.createServer(function (request, response) {
+
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/html'});
+      // Read the requested file content from file system
+      fs.readFile("./index.html", function (err, data) {
+        if (err) {
+           console.log(err);
+           // HTTP Status: 404 : NOT FOUND
+           // Content Type: text/plain
+           response.writeHead(404, {'Content-Type': 'text/html'});
+        }else {	
+           //Page found	  
+           // HTTP Status: 200 : OK
+           // Content Type: text/plain
+           response.writeHead(200, {'Content-Type': 'text/html'});	
+           
+           // Write the content of the file to response body
+           response.write(data.toString());		
+        }
+        // Send the response body 
+        response.end();
+     });   
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
